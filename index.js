e2301ef8a3e50e78e1a1236d1e071c5a2575633c
Rahul Gandhi <rahulgandhi241172@gmail.com>
2023-03-24 22:30:24 +0530

    fix

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const express = require('express');
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+});
+
+
+
+
+
+
+app.listen(8080,()=>{
+    console.log('server is running')
+})
